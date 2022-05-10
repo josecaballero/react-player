6fb8987ea9bde6887e27f0eff021d1e097401d1b
Jose <jlcab8@gmail.com>
2022-05-10 19:28:56 +0200

    added node-sass & font-awesome

---
+++ src/components/Player.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+
+      <div className="play-control">
+        <FontAwesomeIcon size="2x" className="skip-back" icon={faAngleLeft} />
+        <FontAwesomeIcon size="2x" className="play" icon={faPlay} />
+        <FontAwesomeIcon
+          size="2x"
+          className="skip-forward"
+          icon={faAngleRight}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
